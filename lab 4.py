6990b3fbde2069ab1f2882a5fa700e8bc731a02a
ElizavetaSalinaR3138 <113430764+ElizavetaSalinaR3138@users.noreply.github.com>
2022-11-10 00:54:29 +0300

    Add files via upload

--- lab 4.py
+++ lab 4.py
@@ -6,47 +6,35 @@ window.geometry('960x720')
 
 # Генерация ключа
 import random
-kit1 = []
-kit2 = []
-kit_ = '1234567890'
-for i in kit_:
-    kit1.append(i)
-kit_= 'ABCDEFGHIGKLMNOPQRSTUVWXYZ'
-for i in kit_:
-    kit2.append(i)
+kit1 = list('1234567890')
+kit2= list('ABCDEFGHIGKLMNOPQRSTUVWXYZ')
 
-def generator_key():
-    key1_array = []
+def key_generator():
+    key1_parts = []
     for i in range(2):
-        key1_array.append(random.choice(kit1))
+        key1_parts.append(random.choice(kit1))
     for i in range(3):
-        key1_array.append(random.choice(kit2))
-    random.shuffle(key1_array)
-    key1 = ''
-    for i in key1_array:
-        key1 += i
-    key2_array = []
+        key1_parts.append(random.choice(kit2))
+    random.shuffle(key1_parts)
+    key1 = ''.join(key1_parts)
+    key2_parts = []
     for i in range(2):
-        key2_array.append(random.choice(kit1))
+        key2_parts.append(random.choice(kit1))
     for i in range(3):
-        key2_array.append(random.choice(kit2))
-    random.shuffle(key1_array)
-    key2 = ''
-    for i in key2_array:
-        key2 += i
-    key3_array = []
+        key2_parts.append(random.choice(kit2))
+    random.shuffle(key2_parts)
+    key2 = ''.join(key2_parts)
+    key3_parts = []
     for i in range(2):
-        key3_array.append(random.choice(kit1))
+        key3_parts.append(random.choice(kit1))
     for i in range(3):
-        key3_array.append(random.choice(kit2))
-    random.shuffle(key1_array)
-    key3 = ''
-    for i in key3_array:
-        key3 += i
+        key3_parts.append(random.choice(kit2))
+    random.shuffle(key3_parts)
+    key3 = ''.join(key3_parts)
     return key1 + '-' + key2 + '-' + key3
 
-def gen_key():
-    field_key.configure(text = generator_key(), font = ('Arial', 16))
+def key_output():
+    field_key.configure(text = key_generator(), font = ('Arial', 16))
 
 # Добавление картинки на фон
 Image = tkinter.PhotoImage(file = 'бомбит.png')
@@ -59,7 +47,8 @@ frame.place(relx= 0.5, rely = 0.5, anchor ='center')
 field_key = tkinter.Label(frame, width= 20)
 field_key.grid(column=1, row = 2, padx= 0, pady = 0)
 
-enter_key = tkinter.Button(frame, text ='Сгенерировать ключ', font = ('Arial', 16), command = gen_key, bg = '#C71585')
+enter_key = tkinter.Button(frame, text ='Сгенерировать ключ', font = ('Arial', 16),
+                           command = key_output, bg = '#C71585')
 enter_key.grid(column=20, row = 3)
 
 window.mainloop()
